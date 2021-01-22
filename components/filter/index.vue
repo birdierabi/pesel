2dbf72401497c91505c24cf7fc4f2e1d2ff9763f
birdierabi <vnefremova8@gmail.com>
2021-01-22 18:00:23 +0300

    fead: add filter component

---
+++ components/filter/index.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+  button.filter-component.flex.j-center.a-center
+      span Породы
+      iconDownArrow
+</template>
+
+<script>
+import iconDownArrow from '@/components/icons/down-arrow'
+
+export default {
+  name: 'filter-component',
+  components: {
+    iconDownArrow
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .filter-component {
+    padding-bottom: 1px;
+
+    font-family: $font-family-default;
+    font-size: $font-size-default;
+    font-weight: $font-weight-normal;
+    line-height: 20px;
+
+    color: $color-text-regular;
+
+    border-bottom: 0.5px dashed $color-text-regular;
+    background-color: inherit;
+
+    cursor: pointer;
+
+    span {
+      margin-right: 10px;
+    }
+
+    svg {
+      width: 5px;
+      height: auto;
+
+      color: $color-text-regular;
+    }
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,23 @@
 <template lang="pug">
-  .page.index
-    div index page
+  .page.index.wrapper
+    div.flex.j-between.a-center
+      vFilter
 </template>
 
 <script>
+
+import vFilter from '@/components/filter'
+
 export default {
-  name: 'page-index'
+  name: 'index-page',
+  components: {
+    vFilter
+  }
 }
 </script>
 
 <style lang="scss" scoped>
   .index {
-    //
+    padding-top: 50px;
   }
 </style>
