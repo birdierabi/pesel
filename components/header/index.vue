3c1646c6a2bc0643a09d8dd855b1ffbcff4f21bb
birdierabi <vnefremova8@gmail.com>
2021-01-22 12:37:31 +0300

    feat: add header

--- components/header/index.vue
+++ components/header/index.vue
@@ -1,16 +1,47 @@
 <template lang="pug">
-  header.header-component
-    div header
+  header.header-component.flex.j-between.a-center.wrapper
+    nuxt-link(:to="'/'").flex
+      iconLogo
+    a.button-component.favourite.flex.a-center(href="/favourites" target="_self" rel="noreferrer")
+      span Избранные пёсели
+      iconHeart
 </template>
 
 <script>
+import iconLogo from '@/components/icons/logo'
+import iconHeart from '@/components/icons/heart'
+
 export default {
-  name: 'header-component'
+  name: 'header-component',
+  components: {
+    iconLogo,
+    iconHeart
+  }
 }
 </script>
 
 <style lang="scss" scoped>
   .header-component {
-    //
+    padding-top: 35px;
+    padding-bottom: 38px;
+
+    color: $color-text-shadow;
+
+    background-color: #151419;
+
+    box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.55);
+
+    .favourite {
+      svg {
+        width: 16px;
+        height: 14px;
+
+        fill: $color-text-shadow;
+      }
+
+      span {
+        margin-right: 10px;
+      }
+    }
   }
 </style>
