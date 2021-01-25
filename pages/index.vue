e93741263469eb69b107638584f0116d65db3010
birdierabi <vnefremova8@gmail.com>
2021-01-25 12:29:58 +0300

    feat: add index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,28 @@
 <template lang="pug">
   .page.index.wrapper
-    div.flex.j-between.a-center
+    div.top.flex.a-start
       vFilter
+      vSorting
+    div.list
+      vCard.big
+      vCard
+      vCard
+      vCard
+      vCard
 </template>
 
 <script>
 
 import vFilter from '@/components/filter'
+import vSorting from '@/components/sorting'
+import vCard from '@/components/card'
 
 export default {
   name: 'index-page',
   components: {
-    vFilter
+    vFilter,
+    vSorting,
+    vCard
   }
 }
 </script>
@@ -19,5 +30,26 @@ export default {
 <style lang="scss" scoped>
   .index {
     padding-top: 50px;
+    padding-bottom: 50px;
+
+    .list {
+      display: grid;
+      grid-template-columns: 1fr 1fr 1fr;
+      grid-column-gap: 30px;
+      grid-row-gap: 30px;
+
+      padding-top: 25px;
+
+      .big {
+        grid-column-start: 1;
+        grid-column-end: 4;
+      }
+    }
+  }
+
+  .top {
+    position: relative;
+
+    padding-bottom: 25px;
   }
 </style>
