653606c15b7db77badc846f3b2b88f57d341c0ec
birdierabi <vnefremova8@gmail.com>
2021-01-25 12:28:22 +0300

    fix: filter component

--- components/filter/index.vue
+++ components/filter/index.vue
@@ -1,45 +1,90 @@
 <template lang="pug">
-  button.filter-component.flex.j-center.a-center
+  details.filter-component
+    summary.flex.a-center
       span Породы
       iconDownArrow
+    div
+      itemComponent.all
+      div.filter-list.flex.wrap
+        span A
+        itemComponent
+        itemComponent
+        itemComponent
+        span B
+        itemComponent
+        itemComponent
+        itemComponent
 </template>
 
 <script>
-import iconDownArrow from '@/components/icons/down-arrow'
+import iconDownArrow from '@/components/icons/down-dir'
+import itemComponent from '@/components/filter-item'
 
 export default {
   name: 'filter-component',
   components: {
-    iconDownArrow
+    iconDownArrow,
+    itemComponent
   }
 }
 </script>
 
 <style lang="scss" scoped>
   .filter-component {
-    padding-bottom: 1px;
-
-    font-family: $font-family-default;
-    font-size: $font-size-default;
-    font-weight: $font-weight-normal;
-    line-height: 20px;
-
-    color: $color-text-regular;
-
-    border-bottom: 0.5px dashed $color-text-regular;
-    background-color: inherit;
+    &[open] summary svg {
+      transform: rotate(180deg);
+    }
 
     cursor: pointer;
 
-    span {
-      margin-right: 10px;
+    summary {
+      max-width: 81px;
+      padding-bottom: 2px;
+
+      span {
+        line-height: 20px;
+        border-bottom: 0.5px dashed $color-white;
+        margin-right: 10px;
+      }
+
+      svg {
+        width: 9px;
+        height: auto;
+
+        color: $color-white;
+      }
     }
 
-    svg {
-      width: 5px;
-      height: auto;
+    summary::-webkit-details-marker {
+      display: none;
+    }
 
-      color: $color-text-regular;
+    div {
+      padding-top: 30px;
+    }
+
+    .all {
+      border: 1px solid $color-blue;
+      color: $color-blue;
+    }
+
+    .filter-list {
+      display: flex;
+      align-items: center;
+      gap: 15px;
+      padding-top: 15px;
+      padding-bottom: 15px;
+
+      span {
+        display: block;
+
+        font-size: 20px;
+        color: $color-text-shadow;
+      }
+
+      span:not(:first-child) {
+        margin-left: 40px;
+      }
     }
   }
 </style>
