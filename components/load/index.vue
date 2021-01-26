a9c774963d5adfd208756a588972af4fe98f1b0b
birdierabi <vnefremova8@gmail.com>
2021-01-26 12:00:50 +0300

    feat: add loading

---
+++ components/load/index.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+  .load-component.flex.a-center
+    span
+    span
+    span.big
+</template>
+
+<script>
+
+export default {
+  name: 'load-component',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .load-component {
+    gap: 15px;
+
+    span {
+      width: 12px;
+      height: 12px;
+      border: 1px solid #FFFFFF;
+      border-radius: 50%;
+      background-color: inherit;
+      display: flex;
+
+      &.big {
+        width: 16px;
+        height: 16px;
+      }
+    }
+  }
+</style>
