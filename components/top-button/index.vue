4033b58e2b441224d5609ee207ddcab7b46d2642
birdierabi <vnefremova8@gmail.com>
2021-01-26 12:01:12 +0300

    feat: add top button

---
+++ components/top-button/index.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  button.up-button-component.flex.j-center.a-center
+    vArrow
+</template>
+
+<script>
+
+import vArrow from '@/components/icons/arrow'
+
+export default {
+  name: 'up-button-component',
+  components: {
+    vArrow
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .up-button-component {
+    width: 55px;
+    height: 55px;
+
+    background-color: transparent;
+    border: 1px solid $color-white;
+    border-radius: 50%;
+
+    cursor: pointer;
+
+    svg {
+      width: 12.25px;
+      height: auto;
+
+      color: $color-white;
+    }
+  }
+</style>
