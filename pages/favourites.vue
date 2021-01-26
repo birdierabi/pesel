3e89df13c1ec1c57d9004b270a19347eb8631748
birdierabi <vnefremova8@gmail.com>
2021-01-26 12:07:38 +0300

    feat: add favourites page

---
+++ pages/favourites.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  .page.favourites-page.wrapper
+    div.list
+      vCard
+      vCard
+      vCard
+</template>
+
+<script>
+
+import vCard from '@/components/card'
+
+export default {
+  name: 'favourites-page',
+  components: {
+    vCard
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .favourites-page {
+    padding-top: 122px;
+
+    .list {
+      display: grid;
+      grid-template-columns: 1fr 1fr 1fr;
+      grid-column-gap: 30px;
+      grid-row-gap: 30px;
+    }
+  }
+</style>
